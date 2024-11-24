b490698adfaa78df97f6c8fdc532c6daedfea9de
Ganesh H N <ganesh.hn@operaative.com>
2024-11-24 13:33:11 +0530

    SeconCommit

--- Snapdeal/src/test/java/com/tutorialsNinja/IntializeBrowser.java
+++ Snapdeal/src/test/java/com/tutorialsNinja/IntializeBrowser.java
@@ -1,5 +1,17 @@
 package com.tutorialsNinja;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
 public class IntializeBrowser {
+	public static WebDriver driver=null;
+	
+	@BeforeTest
+	public void LaunchBrowser()
+	{
+		driver=new ChromeDriver();
+		
+	}
 
 }
